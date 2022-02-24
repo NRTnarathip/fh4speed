00230a91e02a9e1800a6dc29dbc274670f702cde
narathipgamecaster@gmail.com <61742272+NRTnarathip@users.noreply.github.com>
2022-02-24 20:18:54 +0700

    update eadme,fxmanifest

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -13,21 +13,15 @@ shared_scripts  {
 ui_page "html/hud.html"
 
 files {
-	"html/hud.html",
-	"html/js/gauge.min.js",
-	"html/js/hud.js",
-	"html/css/hud.css",
-	"html/fonts/Oswald-Light.eot",
-	"html/fonts/Oswald-Light.svg",
-	"html/fonts/Oswald-Light.ttf",
-	"html/fonts/Oswald-Light.woff",
-	"html/fonts/Oswald-Light.woff2",
-	"html/fonts/Oswald-Regular.eot",
-	"html/fonts/Oswald-Regular.svg",
-	"html/fonts/Oswald-Regular.ttf",
-	"html/fonts/Oswald-Regular.woff",
-	"html/fonts/Oswald-Regular.woff2",
-	"html/images/speedcircle.png",
+	"html/*.html",
+	"html/js/*.js",
+	"html/css/*.css",
+	"html/fonts/*.eot",
+	"html/fonts/*.svg",
+	"html/fonts/*.ttf",
+	"html/fonts/*.woff",
+	"html/fonts/*.woff2",
+	"html/images/*.png",
 }
 
 client_scripts {
@@ -35,4 +29,7 @@ client_scripts {
 } 
 server_scripts {
 	"server.lua"
+}
+dependencies {
+	'es_extended',
 }
